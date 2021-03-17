1a0124cfee229898a07ec69483047cb156739156
ali <a.khadim@xpay.de>
2021-03-17 16:33:52 +0100

    initial comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My App`,
+    description: `Responsive navigation bar built with Semantic-ui-react in Gatsby`,
+    author: `ALI-DEV`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,16 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
+//@ts-check
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import { Sidebar, Menu, Container, Button, Segment } from "semantic-ui-react"
+
+import Navbar from "./navbar"
+import NavLinks from "./navLinks"
 
 const Layout = ({ children }) => {
+  const [visible, setVisible] = React.useState(false)
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -24,27 +22,33 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
+    <Sidebar.Pushable>
+      <Sidebar
+        as={Menu}
+        animation="push"
+        color="blue"
+        inverted
+        vertical
+        visible={visible}
+        width="thin"
       >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
+        <Button.Group vertical style={{ marginTop: "3rem" }}>
+          <NavLinks />
+        </Button.Group>
+      </Sidebar>
+      <Sidebar.Pusher dimmed={visible}>
+        <Navbar
+          siteTitle={data.site.siteMetadata?.title}
+          setVisible={setVisible}
+          visible={visible}
+        />
+        <Container as="main">{children}</Container>
+        <Segment attached color="blue" inverted textAlign="center" as="footer">
           © {new Date().getFullYear()}, Built with
-          {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+        </Segment>
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
   )
 }
 
